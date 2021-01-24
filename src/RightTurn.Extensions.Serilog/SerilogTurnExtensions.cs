a2d14223a9dd88e5c32f2b0ed189ded0276ff687
Matt Janda <matt.janda.kingston@gmail.com>
2021-01-24 23:08:49 +0000

    feat: Add turn serilog extensions

---
+++ src/RightTurn.Extensions.Serilog/SerilogTurnExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RightTurn.Extensions.Logging;
+using Serilog;
+using System;
+
+namespace RightTurn.Extensions.Serilog
+{
+    public static class SerilogTurnExtensions
+    {
+        public static ITurn WithSerilog(this ITurn turn) => WithSerilog(turn, (configuration) => configuration.ReadFrom.Configuration(turn.Directions.Get<IConfiguration>()));
+
+        public static ITurn WithSerilog(this ITurn turn, Func<LoggerConfiguration, LoggerConfiguration> configuration)
+        {
+            turn.Directions.Add<Action<ILoggingBuilder>>((ILoggingBuilder builder) =>
+            {
+                builder.AddSerilog(
+                    configuration
+                        .Invoke(new LoggerConfiguration())
+                        .CreateLogger(),
+                    dispose: true);
+            });
+
+            if (!turn.Directions.Have<ITurnLogging>())
+                turn.WithLogging();
+
+            return turn;
+        }
+    }
+}
