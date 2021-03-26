504ad9b779302ea826e2d46d375b35d338723cf2
Matt Janda <matt.janda.kingston@gmail.com>
2021-03-26 23:56:22 +0000

    feat: Use turn logging interface to handle serilog directions

---
+++ src/RightTurn.Extensions.Serilog/TurnSerilogLogging.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace RightTurn.Extensions.Serilog
+{
+    class TurnSerilogLogging : ITurnLogging
+    {
+        readonly Action<ILoggingBuilder> _logging;
+
+        public TurnSerilogLogging(Action<ILoggingBuilder> logging)
+        {
+            _logging = logging;
+        }
+
+        /// <summary>
+        /// This method is called from Turn.Take to add logging with logging builder. 
+        /// You must add Action<ILoggingBuilder> to direction container using WithLogging extensions.
+        /// </summary>
+        /// <param name="turn">Turn instance</param>
+        public void AddLogging(ITurn turn)
+        {
+            turn.Directions.ServiceCollection().AddLogging(_logging);
+        }
+    }
+}
